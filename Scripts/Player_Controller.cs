0055d5c45a0e23667eface5652cd0634d74ab248
mln-grge <mgeorge00747@gmail.com>
2020-09-23 00:17:11 +0530

    pushing Mini-Assignment 2

---
+++ Scripts/Player_Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    private Animator anim;
+    private float move;
+    private bool isfacingright = true;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        move = Input.GetAxisRaw("Horizontal");
+        
+        if (move > 0 && !isfacingright)
+            Flip();
+        else if (move < 0 && isfacingright)
+            Flip();
+        
+        
+        Animations();
+    }
+
+    private void Flip()
+    {
+        isfacingright = !isfacingright;
+        Vector3 thescale = transform.localScale;
+        thescale.x *= -1;
+        transform.localScale = thescale;
+    }
+    private void Animations()
+    {
+        anim.SetFloat("speed", Mathf.Abs(move));
+    }
+}
