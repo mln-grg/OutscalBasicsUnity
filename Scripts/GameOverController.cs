fe3ea3cca35a4e7c4de62a54f5ec86e56b9a7922
mln-grge <mgeorge00747@gmail.com>
2020-10-13 10:52:08 +0530

    Implemented a level manager system with which a levels are locked or unlocked . Scripts LevelManager,LevelLoader&LevelStatus used to implement said feature.

---
+++ Scripts/GameOverController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    public Button RestartButton;
+    public Button MainMenuButton;
+
+    private void Awake()
+    {
+        RestartButton.onClick.AddListener(ReloadLevel);
+        MainMenuButton.onClick.AddListener(MainMenu);
+    }
+    public void GameOver()
+    {
+        gameObject.SetActive(true);
+    }
+    private void ReloadLevel()
+    {
+        int level;
+        level = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(level);
+    }
+    private void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Scripts/LevelS/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager instance;
+    public static LevelManager Instance { get { return instance; } }
+    public string level1;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        if(GetLevelStatus(level1) == LevelStatus.Locked)
+        {
+            SetLevelStatus(level1, LevelStatus.Unlocked);
+        }
+    }
+    public LevelStatus GetLevelStatus(string level)
+    {
+        LevelStatus levelStatus = (LevelStatus)PlayerPrefs.GetInt(level, 0);
+        return levelStatus;
+    }
+    public void SetLevelStatus(string level, LevelStatus levelstatus)
+    {
+        PlayerPrefs.SetInt(level, (int)levelstatus);
+    }
+}
